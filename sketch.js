7750d9da98266c56521bd8cb3699da72b103d2e4
Arek Ouzounian <agouzo777@gmail.com>
2019-02-11 09:52:41 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var angle = 0;
+var slider;
+
+function setup() {
+  createCanvas(1550, 600);
+  slider = createSlider(0, TWO_PI, PI / 9, 0.01);
+}
+
+function draw() {
+  background(51);
+  angle = slider.value();
+  var length = 100;
+  stroke(255);
+  translate(750, height);
+  branch(length);
+}
+
+function branch(length) {
+  line(0, 0, 0, -length);
+  translate(0, -length);
+  if(length > 10) {
+    push();
+    rotate(angle);
+    branch(length *.8);
+    pop();
+    push();
+    rotate(-angle);
+    branch(length * .8);
+    pop();
+  }
+}
